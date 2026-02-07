05aa863edc11e54fcf189a3036d03a65a7ed99da
muhin119 <jakmuhin795@gmail.com>
2026-02-07 14:26:21 +0600

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'MUHIN.WAVE | Digital Marketing Manager',
+  description: 'Expert SEO and Ads for UK & BD.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
